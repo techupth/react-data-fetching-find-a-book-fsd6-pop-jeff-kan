18abe0b20666e93c0e043070fdb9f000b0d41502
Jeffiros <krittiwat.c@gmail.com>
2024-06-07 16:06:49 +0700

    feat: fill up answer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [text, setText] = useState("");
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    findBook();
+  },[text]);
+
+  const findBook = async () => {
+    try {
+      {
+        const result = await axios.get(
+          `https://www.googleapis.com/books/v1/volumes?q=${text}`
+        );
+        setBook(result.data.items);
+      }
+    } catch (error) {
+      console.error("Fail to search");
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Find a book</h1>
+      <label className="SearchTab">
+        <input
+          type="text"
+          placeholder="Search here"
+          value={text}
+          onChange={(event) => {
+            setText(event.target.value);
+          }}
+        />
+      </label>
+      <div className="SearchResult">
+        <ul className="SearchList">
+          
+          {book.map((item, index) => {
+            return <li className="List" key={index}>{item.volumeInfo.title}</li>;
+          })}
+
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
