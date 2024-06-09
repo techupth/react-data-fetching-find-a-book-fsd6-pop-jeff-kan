ea7791b35d64af4094dd0435b5702ea2ece8679a
Kan <kanalovefootball@gmail.com>
2024-06-09 16:53:14 +0700

    Feat: Search book feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [searchBook, setSearchBook] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    if (searchBook) {
+      searchFromServer(searchBook);
+    }
+  }, [searchBook]);
+
+  const searchFromServer = async (text) => {
+    try {
+      const response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${text}`
+      );
+      setSearchResult(response.data.items);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input type="text" onChange={(e) => setSearchBook(e.target.value)} />
+      {searchResult.length > 0 && (
+        <>
+          {searchResult.map((result, index) => (
+            <div key={index} className="autocompleteItems">
+              <li className="search-result">{result.volumeInfo.title}</li>
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
